4c21ae9bb39570882a0ae031d45e34982508b35d
piyushpanchal114 <piyushpanchal114@gmail.com>
2023-08-03 21:49:01 +0530

    form validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,50 @@
-import { useForm } from "react-hook-form";
+import { FieldValues, FormState, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 import "./App.css";
 
-enum CategoryEnum {
-  groceries = "Groceries",
-  utilities = "Utilities",
-  entertainment = "Entertainment",
+enum Category {
+  none = "",
+  groceries = "groceries",
+  utilities = "utilities",
+  entertainment = "entertainment",
 }
 
-interface FormInput {
-  description: String;
-  amount: Number;
-  category: CategoryEnum;
-}
+// const CategoryEnum = z.nativeEnum(Category);
+
+const schema = z.object({
+  description: z
+    .string()
+    .min(3, { message: "Description should be at least 3 characters." }),
+  amount: z.number({
+    required_error: "Amount is required",
+    invalid_type_error: "Amount must be a number",
+  }),
+  category: z.nativeEnum(Category),
+});
+
+type FormData = z.infer<typeof schema>;
 
 function App() {
-  const { register, handleSubmit } = useForm<FormInput>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  });
+
+  const handleForm = (data: FieldValues) => console.log(data);
+
   return (
     <>
-      <form action="">
+      <form className="needs-validation" onSubmit={handleSubmit(handleForm)}>
         <div className="mb-3">
           <label className="form-label" htmlFor="">
             Description
           </label>
           <input className="form-control" id="" {...register("description")} />
+          {errors.description && <p>{errors.description.message}</p>}
         </div>
 
         <div className="mb-3">
@@ -35,17 +57,23 @@ function App() {
             id=""
             {...register("amount")}
           />
+          {errors.amount && <p>{errors.amount.message}</p>}
         </div>
         <div className="mb-3">
           <label className="form-label" htmlFor="">
             Category
           </label>
           <select className="form-select" {...register("category")}>
+            <option value="none"></option>
             <option value="groceries">Groceries</option>
             <option value="utilities">Utilities</option>
             <option value="entertainment">Entertainment</option>
           </select>
+          {errors.category && <p>{errors.category.message}</p>}
         </div>
+        <button className="btn btn-primary" type="submit">
+          Submit
+        </button>
       </form>
     </>
   );
