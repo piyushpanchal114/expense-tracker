b34781e5b6137434f767811b531becc90bdca1ea
piyushpanchal114 <piyushpanchal114@gmail.com>
2023-08-07 21:46:02 +0530

    .

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import "./App.css";
 import Button from "./components/Button";
 import { useRef, useState } from "react";
 import ExpenseList from "./components/expense-tracker/components/ExpenseList";
+import ExpenseFilter from "./components/expense-tracker/components/ExpenseFilter";
+import ExpenseForm from "./components/expense-tracker/components/ExpenseForm";
+import categories from "./components/expense-tracker/categories";
 
 enum Category {
   none = "",
@@ -29,11 +32,12 @@ const schema = z.object({
 type FormData = z.infer<typeof schema>;
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState("");
   const [expenses, setExpenses] = useState([
-    { id: 1, description: "aaa", amount: 10, category: "entertainment" },
-    { id: 2, description: "bb", amount: 20, category: "entertainment" },
-    { id: 3, description: "aacca", amount: 30, category: "entertainment" },
-    { id: 4, description: "dd", amount: 10, category: "entertainment" },
+    { id: 1, description: "aaa", amount: 10, category: "Entertainment" },
+    { id: 2, description: "bb", amount: 20, category: "Entertainment" },
+    { id: 3, description: "aacca", amount: 30, category: "Entertainment" },
+    { id: 4, description: "dd", amount: 10, category: "Entertainment" },
   ]);
 
   const {
@@ -59,10 +63,22 @@ function App() {
     console.log(data.target.value);
   };
 
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((e) => e.category === selectedCategory)
+    : expenses;
+
   return (
     <>
+      <div className="mb-5">
+        <ExpenseForm />
+      </div>
+      <div className="mb-3">
+        <ExpenseFilter
+          onSelectCategory={(category) => setSelectedCategory(category)}
+        />
+      </div>
       <ExpenseList
-        expenses={expenses}
+        expenses={visibleExpenses}
         onDelete={(id) =>
           setExpenses(expenses.filter((expense) => expense.id !== id))
         }
